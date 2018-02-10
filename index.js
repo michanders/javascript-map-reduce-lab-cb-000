71f54573e5eaceeab94de4980015b6bd881cfc64
Michael Anderson <michanders@msn.com>
2018-02-10 20:53:19 +0000

    Done.

--- index.js
+++ index.js
@@ -9000,3 +9000,33 @@ const issues = [
     "url": "https://api.github.com/repos/learn-co-curriculum/js-donut-lab/issues/2"
   }
 ];
+
+function add(a,b){
+  return a+b;
+};
+
+var issuesWithUpdatedApiUrl = issues.map(issue => {
+  var r = issue.url.split(".");
+  r[0] += "-v2";
+  return Object.assign({}, issue, {
+    url: r.join(".")
+  })
+});
+
+var commentCountAcrossIssues = issues.map(issue => {
+  return issue.comments_count;
+}).reduce(add,0);
+
+var openIssues = issues.filter(issue => issue.state === "open");
+
+var nonAutomaticIssues = issues.filter(issue => issue.body !== "This pull request has been automatically created by learn.co.");
+
+var results = document.getElementById("results");
+
+
+results.innerHTML = nonAutomaticIssues.map(issue =>
+  `<tr>
+    <td>${issue.body}</td>
+    <td>${issue.created_at}</td>
+    <td>${issue.state}</td>
+  </tr>`).join('');
